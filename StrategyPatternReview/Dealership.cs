2765f0983afbc88c625a7b80af80161aebbd3fc2
Victor Olukayode Olure <victorolure@gmail.com>
2022-10-26 05:21:54 -0500

    Add project files.

---
+++ StrategyPatternReview/Dealership.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternReview
+{
+
+    //DECORATOR PATTERN
+    //Use Case: Base class can be optionally modified at runtime, possibly multiple times
+    //DESIGN
+    //Need a abstract class that declares abstract methods tha we expect to have on the base class
+    // and be modified by decorators.
+    //Need a concrete class that at least implements the abstract base clas methods.
+    //Need an Abstract decorator class which has aproperty referring to an instance of the base Abstract
+    // class, and overrides of the base abstract class methods.
+
+    //When using this, we need to first instantiate a concrete class.
+    //We can then redefine the reference to that instance of the concrete class as an instance of the decorator,
+    // with reference back to the previously referred instance.
+        
+     
+    public abstract class Car
+    {
+        protected string _upgrades;
+
+        public double Cost;
+
+        public string Description;
+        public abstract double GetTotal();
+        public abstract string GetDescription();
+    }
+
+    public class BaseVehicle : Car
+    {
+        public DateTime Year { get; set; } //year wont change
+        public void SetYear(DateTime time)
+        {
+            Year = time;
+        }
+
+        public string Model { get; set; }
+        public double BasePrice { get; set; }
+        public string Colour { get; set; }
+        public string BodyType { get; set; }
+
+        public override double GetTotal()
+        {
+            return BasePrice;
+        }
+
+        public BaseVehicle(int year, string model, double baseprice,string colour, string bodyType)
+        {
+            Year = new DateTime(year, 1,1);
+            Model = model;
+            BasePrice = baseprice;
+            BodyType = bodyType;
+            Colour = colour;
+        }
+
+        public override string GetDescription()
+        {
+            return $"{Colour} {Year.Year} Honda {Model} {BodyType}";
+        }
+    }
+
+    public abstract class UpgradeDecorator: Car
+    {
+        public Car Car;
+
+        public override double GetTotal()
+        {
+            return Car.GetTotal() + Cost;
+        }
+        public override string GetDescription()
+        {
+            return $"{Car.GetDescription()}, {Description}";
+        }
+    }
+
+    public class HeatedSeats : UpgradeDecorator
+    {
+        public HeatedSeats(Car car)
+        {
+            Car = car;
+            Cost = 3000;
+            Description = "Heated Seats";
+        }
+
+    }
+
+    public class RemoteStart : UpgradeDecorator
+    {
+        public RemoteStart(Car car)
+        {
+            Car = car;
+            Cost = 2000;
+            Description = "Remote Start";
+        }
+        public override string GetDescription()
+        {
+            return $"{Car.GetDescription()}, {Description}";
+        }
+
+        public override double GetTotal()
+        {
+            return Car.GetTotal() + Cost;
+        }
+    }
+
+    public class AlloyRims : UpgradeDecorator
+    {
+        public AlloyRims(Car car)
+        {
+            Car = car;
+            Cost = 1000;
+            Description = "Alloy Rims";
+        }
+    }
+}
---
+++ StrategyPatternReview/Program.cs
@@ -0,0 +1,146 @@
+﻿using StrategyPatternReview;
+// abstract beverage class
+//Beverage testCoffeee = new DripCoffee();
+//Console.WriteLine(testCoffeee.GetDescription());
+//Console.WriteLine(testCoffeee.Cost());
+
+//testCoffeee = new Sugar(testCoffeee);
+//Console.WriteLine(testCoffeee.GetDescription());
+//Console.WriteLine(testCoffeee.Cost());
+
+//testCoffeee = new Sugar(testCoffeee);
+//Console.WriteLine(testCoffeee.GetDescription());
+//Console.WriteLine(testCoffeee.Cost());
+
+//testCoffeee = new MilkCondiment(testCoffeee);
+//Console.WriteLine(testCoffeee.GetDescription());
+//Console.WriteLine(testCoffeee.Cost());
+
+//Beverage milk = new MilkBeverage();
+//milk = new MilkCondiment(milk);
+
+//Console.WriteLine(milk.GetDescription());
+//milk = new Sugar(milk);
+//milk = new Sugar(milk);
+//Console.WriteLine(milk.GetDescription());
+//Console.WriteLine(milk.Cost());
+
+
+///last tested calls.
+//Beverage CoffeeBeverage = new DripCoffee();
+//CoffeeBeverage = new Sugar(CoffeeBeverage);
+//CoffeeBeverage = new Sugar(CoffeeBeverage);
+
+//Beverage SecondCoffee = CoffeeBeverage;
+//Beverage ThirdCoffee = CoffeeBeverage;
+
+//ThirdCoffee = new Sugar(ThirdCoffee);
+
+//Console.WriteLine(CoffeeBeverage.GetDescription());
+//Console.WriteLine(SecondCoffee.GetDescription());
+//Console.WriteLine(ThirdCoffee.GetDescription());
+
+//CoffeeBeverage = new Sugar(CoffeeBeverage);
+
+
+//Console.WriteLine(SecondCoffee.Cost());
+//Console.WriteLine(CoffeeBeverage.Cost());
+//Console.WriteLine(ThirdCoffee.Cost());
+
+Car HondaAccord = new BaseVehicle(2020, "Accord", 30000, "Sedan", "Blue");
+Console.WriteLine(HondaAccord.GetTotal());
+Console.WriteLine(HondaAccord.GetDescription());
+
+HondaAccord = new HeatedSeats(HondaAccord);
+HondaAccord = new RemoteStart(HondaAccord);
+
+HondaAccord = new AlloyRims(HondaAccord);
+Console.WriteLine(HondaAccord.GetTotal());
+Console.WriteLine(HondaAccord.GetDescription());
+
+public abstract class Beverage
+{
+    protected string _description { get; set; }
+    public virtual string GetDescription()
+    {
+        return _description;
+    }
+
+    protected double _cost { get; set; }
+    public virtual double Cost()
+    {
+        return _cost;
+    }
+}
+
+public class DripCoffee : Beverage
+{
+    public DripCoffee()
+    {
+        _cost = 1.00;
+        _description = "Columbia Coffee";
+    }
+}
+
+public class Tea : Beverage
+{
+    public Tea()
+    {
+        _cost = 0.50;
+        _description = "English Tea";
+    }
+}
+
+public class MilkBeverage : Beverage
+{
+    public MilkBeverage()
+    {
+        _description = "Milk that you drink";
+        _cost = 2.00;
+    }
+}
+
+public abstract class CondimentDecorator : Beverage
+{
+    public Beverage Beverage { get; set; }
+    public abstract override string GetDescription();
+    public abstract override double Cost();
+}
+
+public class Sugar: CondimentDecorator
+{
+    public override double Cost()
+    {
+        return Beverage.Cost() + _cost;
+    }
+    public override string GetDescription()
+    {
+        return $"{Beverage.GetDescription()}, {_description}";
+    }
+
+    public Sugar(Beverage beverage)
+    {
+        Beverage = beverage;
+        _cost = 0.2;
+        _description = "Sugar";
+    }
+}
+
+public class MilkCondiment : CondimentDecorator
+{
+    public override double Cost()
+    {
+        return Beverage.Cost() + _cost;
+    }
+    public override string GetDescription()
+    {
+        return $"{Beverage.GetDescription()}, {_description}";
+    }
+
+    public MilkCondiment(Beverage beverage)
+    {
+        Beverage = beverage;
+        _cost = 0.15;
+        _description = "Milk";
+    }
+}
